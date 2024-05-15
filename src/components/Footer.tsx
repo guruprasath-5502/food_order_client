a08d1bac3989c2425d680e6c1ff2432f8d959c0f
guruprasath-5502 <guruprasath2552@gmail.com>
2024-05-15 17:35:53 +0530

    css changes

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,10 +2,10 @@ import { Link } from 'react-router-dom';
 
 const Footer = () => {
   return (
-    <div className='bg-orange-500 py-10'>
+    <div className='bg-orange-500 py-3'>
       <div className='container mx-auto flex flex-col md:flex-row justify-between items-center'>
         <Link to='/'>
-          <span className='text-3xl text-white font-bold tracking-tight'>
+          <span className='text-2xl text-white font-bold tracking-tight'>
             Eats.com
           </span>
         </Link>
