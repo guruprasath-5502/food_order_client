21a17ffdd633dfbad927ed061aa333df9b89bf23
Guru Prasath <guruprasath2552@gmail.com>
2024-05-15 17:19:42 +0530

    user data populated and route protected (#4)

--- src/api/MyUserApi.tsx
+++ src/api/MyUserApi.tsx
@@ -1,5 +1,6 @@
+import { User } from '@/types';
 import { useAuth0 } from '@auth0/auth0-react';
-import { useMutation } from 'react-query';
+import { useMutation, useQuery } from 'react-query';
 import { toast } from 'sonner';
 
 const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
@@ -31,16 +32,20 @@ export const useCreateMyUser = () => {
 
   const {
     mutateAsync: createUser,
-    isLoading,
-    isError,
     isSuccess,
+    error,
   } = useMutation(createUserRequest);
+
+  if (isSuccess) {
+    toast.success('User loggedIn');
+  }
+
+  if (error) {
+    toast.error(error.toString());
+  }
 
   return {
     createUser,
-    isError,
-    isLoading,
-    isSuccess,
   };
 };
 
@@ -92,8 +97,46 @@ export const useUpdateMyUser = () => {
   return {
     updateUser,
     isLoading,
-    isSuccess,
+  };
+};
+
+export const useMyGetUser = () => {
+  const { getAccessTokenSilently } = useAuth0();
+
+  const getUserRequest = async (): Promise<User> => {
+    const accessToken = await getAccessTokenSilently();
+
+    const response = await fetch(`${API_BASE_URL}/api/user/myuser`, {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: `Bearer ${accessToken}`,
+      },
+    });
+
+    if (!response.ok) {
+      throw new Error('Failed to fetch user');
+    }
+
+    const resData = await response.json();
+
+    const { data } = resData;
+
+    return Promise.resolve(data);
+  };
+
+  const {
+    data: currentUser,
+    isLoading,
     error,
-    reset,
+  } = useQuery('fetchCurrentUser', getUserRequest);
+
+  if (error) {
+    toast.error(error.toString());
+  }
+
+  return {
+    currentUser,
+    isLoading,
   };
 };
