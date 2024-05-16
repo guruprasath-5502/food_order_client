ed5f6ea4b68d56c1f6bc6155651ef1031c7b3211
guruprasath-5502 <guruprasath2552@gmail.com>
2024-05-16 23:40:30 +0530

    added mobile nav route

--- src/components/MobileNavLinks.tsx
+++ src/components/MobileNavLinks.tsx
@@ -1,17 +1,28 @@
 import { Link } from 'react-router-dom';
 import { Button } from './ui/button';
 import { useAuth0 } from '@auth0/auth0-react';
+import { SheetClose } from './ui/sheet';
 
 const MobileNavLinks = () => {
   const { logout } = useAuth0();
   return (
     <>
-      <Link
-        to='/user-profile'
-        className='flex bg-white items-center font-bold hover:text-orange-500 pl-1'
-      >
-        User Profile
-      </Link>
+      <SheetClose asChild>
+        <Link
+          to='/manage-restaurant'
+          className='flex justify-center items-center font-bold hover:text-orange-500'
+        >
+          Manage Restaurant
+        </Link>
+      </SheetClose>
+      <SheetClose asChild>
+        <Link
+          to='/user-profile'
+          className='flex justify-center items-center font-bold hover:text-orange-500'
+        >
+          User Profile
+        </Link>
+      </SheetClose>
       <Button
         onClick={() => logout()}
         className='flex items-center font-bold px-3 hover:bg-gray-500'
