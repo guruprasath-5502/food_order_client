e859eceacb6c73c4bf29bff4670363b9cc477912
guruprasath-5502 <guruprasath2552@gmail.com>
2024-05-18 21:57:53 +0530

    api fix

--- src/pages/SearchPage.tsx
+++ src/pages/SearchPage.tsx
@@ -74,7 +74,7 @@ const SearchPage = () => {
     return <LoadingSpinner />;
   }
 
-  if (!results?.data.data.length || !city) {
+  if (!results?.status || !city) {
     return (
       <div className='flex flex-col items-center justify-center'>
         <img src={noResultsIcon} alt='No results found' className='w-32' />
