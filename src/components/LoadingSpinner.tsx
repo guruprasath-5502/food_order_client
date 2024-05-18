4bf46bae871ce420529394e0c9733102546749bb
guruprasath-5502 <guruprasath2552@gmail.com>
2024-05-18 20:45:27 +0530

    css

--- src/components/LoadingSpinner.tsx
+++ src/components/LoadingSpinner.tsx
@@ -1,6 +1,6 @@
 const LoadingSpinner = () => {
   return (
-    <div className='scale-20'>
+    <div className='scale-20 flex flex-col items-center justify-center'>
       <div
         className='w-40 h-40 rounded-full shadow-eclipse animate-spin'
         style={{ transformOrigin: '80px 83px' }}
